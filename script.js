2d39e95e3d08d4a0080f6694dfba84bc38e3a6a1
Rib4o0 <121397824+Rib4o0@users.noreply.github.com>
2024-05-30 07:17:22 +0000

    dev commit

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const cells = document.querySelector('.cellContainer').children;
+const cell = document.querySelectorAll('.cell')
+const title = document.querySelector('.title')
+const scoreDisplay = document.querySelector('.score');
+const start = document.querySelector('.start');
 
 console.log(cells[24])
+var inputting = false
 var numbers = [];
+var currentInput = 0;
 
 function generateNewNum() {
+    inputting = false;
+    scoreDisplay.textContent = `Score: ${numbers.length}`
     let num = Math.floor(Math.random() * 24);
     numbers.push(num);
     showNumberCells();
@@ -12,13 +20,15 @@ function generateNewNum() {
 function showNumberCells() {
     let i = 0;
     let interval = setInterval(() => {
-        if (i >= numbers.length) clearInterval(interval);
+        if (i >= numbers.length) {
+            clearInterval(interval)
+            inputting = true;
+            currentInput = 0;
+        }
         showCell(cells[numbers[i]], 1000);
         i++
     }, 1200)
 }
-
-generateNewNum()
 
 function showCell(cell, duration) {
     cell.classList.add('active');
@@ -27,8 +37,32 @@ function showCell(cell, duration) {
     }, duration)
 }
 
-cells.forEach(cell => {
+cell.forEach(cell => {
     cell.addEventListener('click', () => {
-        showCell(cell, 200);
+        if (inputting) {
+            showCell(cell, 200);
+            if (numbers[currentInput] == parseInt(cell.dataset.index)) {
+                if (currentInput < numbers.length - 1 ) currentInput += 1;
+                else generateNewNum();
+            } else {
+                gameOver();
+            }
+        }
     })
+})
+
+function gameOver() {
+    title.textContent = "Game Over";
+    start.classList.remove('hidden')
+    start.classList.add('restarting')
+}
+
+start.addEventListener('click', () => {
+    start.classList.add('hidden')
+    start.textContent = "Restart"
+    if (start.classList.contains('restarting')) {
+        numbers = [];
+        title.textContent = "Memory Game"
+    }
+    generateNewNum()
 })
